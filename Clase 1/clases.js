8496105b7b3a61c37922852edf31a0ed23fe892e
manuelpincay <manuelpincay2001@hotmail.com>
2022-04-23 23:51:58 -0500

    actualizacion clase 2

--- Clase 1/clases.js
+++ Clase 1/clases.js
@@ -7,21 +7,21 @@ const persona = {
         lat:234.234234,
         lng:34.165161,
     },
-    getnombrevompleto(){
+    getnombrevompleto: function(){
         return `${this.nombre} ${this.apellido}`
     }
 }
 
-console.log(persona.getnombrevompleto())
+/* console.log(persona.getnombrevompleto()) */
 
 
-function mostrarDatos({nombre, apellido, geolocalizacion:{lat,lng}})
+function mostrarDatos({nombre, apellido, geolocalizacion:{lat,lng}, getnombrevompleto})
 {
     console.log(nombre)
     console.log(apellido)
     console.log(lat)
     console.log(lng)
-    
+    console.log(getnombrevompleto())
 }
 
 mostrarDatos(persona)
