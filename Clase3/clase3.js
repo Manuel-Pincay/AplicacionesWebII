d4094fcd7077bca166965802285d019b2956b3ad
Manuel-Pincay <e1313614495@live.uleam.edu.ec>
2022-05-05 12:00:31 -0500

    clase 4

--- Clase3/clase3.js
+++ Clase3/clase3.js
@@ -1,6 +1,21 @@
-npm 
+/* npm i mongoose
+npm init -y */
 
-const mongoose = requiere('mongoose');
-const conexion = "mongodb+srv://manuel12pincay:<password>@cluster0.pfnqa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const mongoose = require('mongoose');
+const conexion = "mongodb+srv://manuel12pincay:manuel12pincay@cluster0.pfnqa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-mongoose.connect(conexion)
+
+( async ()=>{/* primero confirmar conexion a la base de datos*/
+const estadodeconexion = await mongoose.connect(conexion);
+/* Creando modelo de la base de datos */
+/* Es opcional crear el usuario, pero recomendable */
+const Usuario = mongoose.model("Usuario", {nombre: String});
+
+const Usuario1 = new Usuario({nombre:"Prueba"});
+
+const guardado = await Usuario1.save();
+
+const resultado = await Usuario.find();
+
+console.log(resultado);
+}) ();
