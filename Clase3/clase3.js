7e484cdc4a6b421130e8cbb10dfee4f3b261f11b
Manuel-Pincay <e1313614495@live.uleam.edu.ec>
2022-05-05 09:42:45 -0500

    clase4

--- Clase3/clase3.js
+++ Clase3/clase3.js
@@ -1,1 +1,6 @@
-const mongoose = requiere(mongosse)
+npm 
+
+const mongoose = requiere('mongoose');
+const conexion = "mongodb+srv://manuel12pincay:<password>@cluster0.pfnqa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+mongoose.connect(conexion)
