4bec15f1f2159f48f1b0225f3bc9eb8de011e815
Manuel-Pincay <103917223+Manuel-Pincay@users.noreply.github.com>
2022-06-23 09:57:39 -0500

    cambios

--- 09CAPAST/dist/index.js
+++ 09CAPAST/dist/index.js
@@ -1,21 +1,25 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
 /*
 npm init --y
 npm i express
 tsc install
 ts */
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
+const server_1 = require("./server");
+const server = new server_1.Server;
+server.listen();
+/* import express from 'express'
+
+const app = express();
 const port = 3000;
-app.get('/', (req, res) => {
+app.get('/',(req,res)=>{
     res.json({
-        msg: 'ok'
-    });
-});
-app.listen(port, () => {
+        msg:'ok'
+    })
+})
+
+app.listen(port, ()=>{
     console.log(`prueba funcionando`);
-});
+}) */ 
--- 09CAPAST/server.ts
+++ 09CAPAST/server.ts
@@ -1,10 +1,51 @@
+import express, {Router, Express} from 'express';
+import cors from 'cors';
+
+import {dbConnection} from './databases/config'
+import {router as producto} from './routes/products'
+
 class Server
 {
-    constructor(){}
-    private async conectarDB(){}
-    private middleware (){}
-    private routes(){}
-    listen(){}
+
+    app: Router;
+    router: Router;
+    port:Number;
+    paths: {[key:string]:string}
+    private _express: Express;
+
+    constructor(){
+        this.app = Router();
+        this.router = Router();
+        this.port = Number(process.env["PORT"]); 
+        this.paths = {
+
+            productos:'/api/productos',
+            clientes:'/api/clientes'
+        }
+        this.conectarDB();
+        this.middleware();
+        this.routes();
+
+        this.router.use('v1/sextoa', this.app);
+        this._express = express().use(this.router) ;
+
+    }
+    private async conectarDB(){
+        await dbConnection();
+    }
+    private middleware (){
+        this.app.use(cors());
+        this.app.use(express.json());
+    }
+    private routes(){
+        this.app.use(this.paths.productos, producto)
+    }
+    
+    listen(){
+        this._express.listen(this.port,() => {
+            console.log(`Server ejecuted on http://localhost:${this.port}/v1/sextoa/api/productos`);
+        })
+    }
 
 }
 
