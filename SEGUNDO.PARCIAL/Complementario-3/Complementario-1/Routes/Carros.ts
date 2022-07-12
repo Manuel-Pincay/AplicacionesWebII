1074c2858ea89cec2227d2098066977bab159e74
Manuel-Pincay <e1313614495@live.uleam.edu.ec>
2022-07-12 01:06:10 -0500

    Complementario3

---
+++ SEGUNDO.PARCIAL/Complementario-3/Complementario-1/Routes/Carros.ts
@@ -0,0 +1,15 @@
+import {Router} from 'express'
+import {check} from 'express-validator'
+import { Carro } from '../Controllers/Index'
+
+const { obtenerCarro, obtenerCarros, crearCarro, actualizarCarro, borrarCarro} = Carro
+
+const router = Router();
+
+router.get('/' , obtenerCarros)
+router.get('/:CARRO_PLACA', obtenerCarro)
+router.post('/', [check('CARRO_PLACA', 'La placa es obligatoria').not().isEmpty()], crearCarro)
+router.put('/:CARRO_PLACA', actualizarCarro)
+router.delete('/:CARRO_PLACA', borrarCarro)
+
+export{router}
--- apuntes.md
+++ apuntes.md
@@ -38,3 +38,27 @@ clientes
 }
 >
 
+# CSR
+
+### Installation Vite Vanilla 
+
+- npm create vite@latest
+        Need to install the following packages:
+        create-vite@latest
+        Ok to proceed? (y) y
+        √ Project name: ... ClienteWeb
+        √ Package name: ... clienteweb
+        √ Select a framework: » vanilla
+        √ Select a variant: » vanilla-ts
+
+        http://localhost:3000/
+
+- Done. Now run:
+
+    cd ClienteWeb
+    npm install
+    npm run dev        
+
+- Install Axios
+
+    npm i axios
